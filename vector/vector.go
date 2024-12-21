db133e35badd017759e2e0168e01f8214e4ecde6
GitSteve1025 <3440346452@qq.com>
2024-12-21 15:16:11 +0800

    add some function of vector

--- vector/vector.go
+++ vector/vector.go
@@ -1,3 +1,52 @@
 package vector
 
 type Vector[T any] []T
+
+func (vec *Vector[T]) Size() int {
+	return len(*vec)
+}
+
+func (vec *Vector[T]) Capacity() int {
+	return cap(*vec)
+}
+
+func (vec *Vector[T]) Empty() bool {
+	return len(*vec) == 0
+}
+
+func (vec *Vector[T]) Front() T {
+	return (*vec)[0]
+}
+
+func (vec *Vector[T]) Back() T {
+	return (*vec)[len(*vec)-1]
+}
+
+func (vec *Vector[T]) Resize(n int) {
+	temp := make(Vector[T], n)
+	copy(temp, *vec)
+	*vec = temp
+}
+
+func (vec *Vector[T]) Assign(n int, val T) {
+	*vec = make(Vector[T], n)
+	for i := range *vec {
+		(*vec)[i] = val
+	}
+}
+
+func (vec *Vector[T]) PushBack(val T) {
+	*vec = append(*vec, val)
+}
+
+// PopBack returns the value of the last element.
+func (vec *Vector[T]) PopBack() T {
+	defer func() { *vec = (*vec)[:len(*vec)-1] }()
+	return (*vec)[len(*vec)-1]
+}
+
+func (vec *Vector[T]) Insert(pos int, val T) {
+	*vec = append(*vec, val)
+	copy((*vec)[pos+1:], (*vec)[pos:])
+	(*vec)[pos] = val
+}
--- vector/vector_test.go
+++ vector/vector_test.go
@@ -1,7 +1,71 @@
 package vector
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestMake(t *testing.T) {
+	vec := make(Vector[int], 10)
+	t.Log(vec, vec.Size(), vec.Capacity(), vec.Empty())
+	vec.Assign(5, 10)
+	t.Log(vec, vec.Size(), vec.Capacity(), vec.Empty())
+	vec.Resize(3)
+	t.Log(vec, vec.Size(), vec.Capacity(), vec.Empty())
+}
 
+func TestFrontBack(t *testing.T) {
+	vec := make(Vector[int], 10)
+	t.Log(vec.Front(), vec.Back())
+}
+
+func TestPushPop(t *testing.T) {
+	vec := make(Vector[int], 5)
+	for i := range vec {
+		vec[i] = i
+	}
+
+	for i := 0; i < 10; i++ {
+		vec.PushBack(i)
+		t.Log(vec, vec.Size(), vec.Capacity(), vec.Empty())
+	}
+
+	for i := 0; i < 15; i++ {
+		vec.PopBack()
+		t.Log(vec, vec.Size(), vec.Capacity(), vec.Empty())
+	}
+}
+
+func TestPushPopEfficiency(t *testing.T) {
+	var vec Vector[int]
+	const N = 10000000
+
+	start := time.Now()
+	for i := 0; i < N; i++ {
+		vec.PushBack(i)
+
+	}
+	t.Log("PushBack", N, "val costs", time.Since(start))
+	// t.Log(vec)
+
+	start = time.Now()
+	for !vec.Empty() {
+		vec.PopBack()
+	}
+	t.Log("PopBack", N, "val costs", time.Since(start))
+	// t.Log(vec)
+}
+
+func TestInsert(t *testing.T) {
+	var vec Vector[int]
+
+	for i := 0; i < 10; i++ {
+		vec.Insert(i, i)
+		t.Log(vec, vec.Size())
+	}
+
+	for i := 0; i < 10; i++ {
+		vec.Insert(10-i, i)
+		t.Log(vec, vec.Size())
+	}
 }
