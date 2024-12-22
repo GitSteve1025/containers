1a367bc10ca14a47b418ba96b238d6f81c56bd5d
GitSteve1025 <3440346452@qq.com>
2024-12-22 22:01:28 +0800

    remake list

--- list/list.go
+++ list/list.go
@@ -1,93 +1,158 @@
+// Copyright (c) 2024 Tecy.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package list
 
 type Element[T any] struct {
+	// The value stored in this element.
 	Value T
-	Prev  *Element[T]
-	Next  *Element[T]
+	// previous pointer of the element.
+	prev *Element[T]
+	// next pointer of the element.
+	next *Element[T]
+	// The list to which this element belongs.
+	list *List[T]
 }
 
-// Doubly linked list.
+func (e *Element[T]) Prev() *Element[T] {
+	if p := e.prev; e.list != nil && p != &e.list.root {
+		return p
+	}
+	return nil
+}
+
+func (e *Element[T]) Next() *Element[T] {
+	if n := e.next; e.list != nil && n != &e.list.root {
+		return n
+	}
+	return nil
+}
+
+// doubly-linked list.
 type List[T any] struct {
-	head *Element[T]
-	tail *Element[T]
+	// sentinel list element, only &root, root.prev, and root.next are used
+	root Element[T]
+	// length of list.
+	size int
 }
 
 // New creates a new empty List[T].
 func New[T any]() *List[T] {
-	var list List[T]
-	return &list
+	list := new(List[T])
+	list.init()
+	return list
+}
+
+// init initializes or clears list.
+func (list *List[T]) init() {
+	list.root.prev = &list.root
+	list.root.next = &list.root
+	list.size = 0
 }
 
 // Front returns the reference of data at the first element of the list.
 func (list *List[T]) Front() *Element[T] {
-	return list.head
+	if list.size != 0 {
+		return list.root.next
+	}
+	return nil
 }
 
 // Back returns the reference of data at the last element of the list.
 func (list *List[T]) Back() *Element[T] {
-	return list.tail
+	if list.size != 0 {
+		return list.root.prev
+	}
+	return nil
 }
 
-// PushBack adds data to the end of the list.
-func (list *List[T]) PushBack(val T) {
-	if list.tail != nil {
-		list.tail.Next = &Element[T]{
-			Value: val,
-			Prev:  list.tail,
-			Next:  nil,
-		}
-		list.tail = list.tail.Next
-	} else {
-		list.tail = &Element[T]{
-			Value: val,
-			Prev:  nil,
-			Next:  nil,
-		}
-		list.head = list.tail
+// insert inserts v before at, increments list.size and returns the element.
+func (list *List[T]) insert(v *Element[T], at *Element[T]) *Element[T] {
+	v.prev = at.prev
+	v.next = at
+	v.list = list
+	at.prev.next = v
+	at.prev = v
+	list.size++
+	return v
+}
+
+// insertValue is a convenience wrapper for insert(&Element[T]{Value: val}, at) and returns the new element.
+func (list *List[T]) insertValue(val T, at *Element[T]) *Element[T] {
+	return list.insert(&Element[T]{Value: val}, at)
+}
+
+// PushBack adds data to the end of the list and returns the new element.
+func (list *List[T]) PushBack(val T) *Element[T] {
+	return list.insertValue(val, &list.root)
+}
+
+// PushFront adds data to the begin of the list and returns the new element.
+func (list *List[T]) PushFront(val T) *Element[T] {
+	return list.insertValue(val, list.root.next)
+}
+
+// InsertBefore inserts val before at, and return the new element.
+// If at is not an element of list, the list is not modified.
+// The at must not be nil.
+func (list *List[T]) InsertBefore(val T, at *Element[T]) *Element[T] {
+	if at.list == list {
+		return list.insertValue(val, at)
 	}
+	return nil
+}
+
+// InsertAfter inserts val after at, and return the new element.
+// If at is not an element of list, the list is not modified.
+// The at must not be nil.
+func (list *List[T]) InsertAfter(val T, at *Element[T]) *Element[T] {
+	if at.list == list {
+		return list.insertValue(val, at.next)
+	}
+	return nil
+}
+
+// erase erases at element.
+func (list *List[T]) erase(at *Element[T]) {
+	at.prev.next = at.next
+	at.next.prev = at.prev
+	at.prev = nil // avoid memory leaks
+	at.next = nil // avoid memory leaks
+	at.list = nil
+	list.size--
 }
 
 // PopBack removes last element and returns the value of the element.
+// It will return default value of T when list is empty.
 func (list *List[T]) PopBack() T {
-	defer func() {
-		list.tail = list.tail.Prev
-		if list.tail != nil {
-			list.tail.Next = nil
-		} else {
-			list.head = nil
-		}
-	}()
-	return list.tail.Value
-}
-
-// PushFront adds data to the begin of the list.
-func (list *List[T]) PushFront(val T) {
-	if list.head != nil {
-		list.head.Prev = &Element[T]{
-			Value: val,
-			Prev:  nil,
-			Next:  list.head,
-		}
-		list.head = list.head.Prev
-	} else {
-		list.head = &Element[T]{
-			Value: val,
-			Prev:  nil,
-			Next:  nil,
-		}
-		list.tail = list.head
+	if list.size > 0 {
+		defer list.erase(list.root.prev)
+		return list.root.prev.Value
 	}
+	var dft T
+	return dft
 }
 
 // PopFront removes the first element and returns the value of the element.
+// It will return default value of T when list is empty.
 func (list *List[T]) PopFront() T {
-	defer func() {
-		list.head = list.head.Next
-		if list.head != nil {
-			list.head.Prev = nil
-		} else {
-			list.tail = nil
-		}
-	}()
-	return list.head.Value
+	if list.size > 0 {
+		defer list.erase(list.root.next)
+		return list.root.next.Value
+	}
+	var dft T
+	return dft
+}
+
+// Erase erases at element and returns the value.
+// If at is not an element of list, the list is not modified.
+// The at must not be nil.
+func (list *List[T]) Erase(at *Element[T]) T {
+	if at.list == list {
+		defer list.erase(at)
+		return at.Value
+	}
+	var dft T
+	return dft
 }
--- list/list_test.go
+++ list/list_test.go
@@ -19,8 +19,8 @@ func TestPushPopBack(t *testing.T) {
 		t.Log(lt.PopBack())
 	}
 
-	if lt.head != nil || lt.tail != nil {
-		t.Error("head or tail is not nil")
+	if lt.Front() != nil || lt.Back() != nil {
+		t.Error("front or back is not nil")
 	}
 }
 
@@ -35,7 +35,88 @@ func TestPushPopFront(t *testing.T) {
 		t.Log(*lt.Front(), *lt.Back(), lt.PopFront())
 	}
 
-	if lt.head != nil || lt.tail != nil {
-		t.Error("head or tail is not nil")
+	if lt.Front() != nil || lt.Back() != nil {
+		t.Error("front or back is not nil")
+	}
+}
+
+func TestForEach(t *testing.T) {
+	lt := New[int]()
+	for i := 0; i < 5; i++ {
+		lt.PushBack(i)
+	}
+
+	a := []int{0, 1, 2, 3, 4}
+	at := []int{}
+	for cur := lt.Front(); cur != nil; cur = cur.Next() {
+		at = append(at, cur.Value)
+	}
+
+	for i := 0; i < 5; i++ {
+		if a[i] != at[i] {
+			t.Error("foreach is wrong")
+		}
+	}
+
+	b := []int{4, 3, 2, 1, 0}
+	bt := []int{}
+	for cur := lt.Back(); cur != nil; cur = cur.Prev() {
+		bt = append(bt, cur.Value)
+	}
+
+	for i := 0; i < 5; i++ {
+		if b[i] != bt[i] {
+			t.Error("rev foreach is wrong")
+		}
+	}
+}
+
+func TestInsertAndErase(t *testing.T) {
+	lt := New[int]()
+	mid := lt.PushBack(1)
+	lt.InsertBefore(0, mid)
+	lt.InsertAfter(2, mid)
+
+	a := []int{0, 1, 2}
+	at := []int{}
+	for cur := lt.Front(); cur != nil; cur = cur.Next() {
+		at = append(at, cur.Value)
+	}
+
+	for i := 0; i < 3; i++ {
+		if a[i] != at[i] {
+			t.Error("insert is wrong")
+		}
+	}
+
+	lt.Erase(mid.Prev())
+	lt.Erase(mid.Next())
+	if lt.size != 1 || lt.Front().Value != 1 {
+		t.Error("erase is wrong")
+	}
+	lt.Erase(mid)
+	if lt.size != 0 {
+		t.Error("erase is wrong")
+	}
+}
+
+func TestWrongInsert(t *testing.T) {
+	a := New[int]()
+	ap := a.PushBack(1)
+	b := New[int]()
+	if b.InsertAfter(0, ap) != nil {
+		t.Error("insertAfter error")
+	}
+	if b.InsertBefore(0, ap) != nil {
+		t.Error("insertBefore error")
+	}
+	if b.PopBack() != 0 {
+		t.Error("popback is error")
+	}
+	if b.PopFront() != 0 {
+		t.Error("popfront is error")
+	}
+	if b.Erase(ap) != 0 {
+		t.Error("erase is error")
 	}
 }
