b86d8d848e234ab8cd163bb7ad4922d998d3aab1
GitSteve1025 <3440346452@qq.com>
2025-01-01 11:49:45 +0800

    Correct the MIT license description.

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2024 Tecy.  All rights reserved.
-// license that can be found in the LICENSE file.
+// Copyright (c) 2024 Tecy.
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
 
 package queue
